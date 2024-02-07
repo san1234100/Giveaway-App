4b0e4c04c36331ef68abc7e9ccb5b43f2c460d01
Sanjeev18207 <u.sanjeev.23cse@psvpec.in>
2024-02-07 13:32:29 +0530

    login authentication

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,10 +8,17 @@ const loginFormData=new FormData(loginFromEl)
 const { email,password }=Object.fromEntries(loginFormData); // object destructuring
 
 loginUser(email,password).then(user=>{
-    console.log('User successfully logged in ',user);
+    // login state in local storage
+    localStorage.setItem('user',JSON.stringify(user));
+
+    window.location.replace('./dashboard.html')
+    // console.log('User successfully logged in ',user);
 })
 .catch(err=>{
-    console.log('Error ',err);
+    // console.log('Error ',err);
+    if(err.errorCode==='auth/internal-error'){
+       alert(`UserName/Password doesn't exist`)
+    }
     loginFromEl.reset();
 })
 // console.log(loginRecord);
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,41 @@ const successfulUIEl=document.getElementById('formSubmitted');
 const formEl=document.querySelector('form');
 console.log(mainEl,successfulUIEl,formEl);
 
-
+const validateForm=()=>{
+    const formInputEls=formEl.querySelectorAll('input , select , textarea'); //To select multiple tags use ,
+  //  console.log(formInputEls);
+  formInputEls.forEach((field)=>{
+     
+      if(!field.hasAttribute('required')){ 
+        field.required=true;
+        return false;
+      }
+  })
+  return true;
+  }
 const submitForm=(event)=>{
-    event.preventDefault();
 
+event.preventDefault();
+//    do form validation
+  if(validateForm()){
     const formData=new FormData(formEl);
-
     // const data=[...formData.entries()];
     // console.log(data);
     const recordObj=Object.fromEntries(formData);
-    // console.log(recordObj);
+    console.log(recordObj);
  createRecord(recordObj);
     // update UI logic
     updateUI();
+  }
+   
 }
+
+
+
 formEl.addEventListener('submit',submitForm)
+
+
+
 const updateUI=()=>{//Update UI
     mainEl.classList.add('hidden')
     successfulUIEl.classList.remove('hidden')
